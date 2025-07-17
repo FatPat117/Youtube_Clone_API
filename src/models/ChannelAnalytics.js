83113f828eb41dc268f207aa15029ebe65fcfe60
Phat117 <ntphat110705@gmail.com>
2025-07-17 23:05:38 +0700

    Channel Analytics Mode

---
+++ src/models/ChannelAnalytics.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const channelAnalyticsSchema = new mongoose.Schema(
+        {
+                channel: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "User",
+                        required: [true, "channel is required"],
+                },
+                totalView: {
+                        type: Number,
+                        default: 0,
+                },
+                totalSubscribers: {
+                        type: Number,
+                        default: 0,
+                },
+                totalVideos: {
+                        type: Number,
+                        default: 0,
+                },
+                totalComments: {
+                        type: Number,
+                        default: 0,
+                },
+                dailyStats: [
+                        {
+                                date: {
+                                        type: Date,
+                                        required: [true, "date is required"],
+                                },
+                                views: {
+                                        type: Number,
+                                        default: 0,
+                                },
+                                subscribersGained: {
+                                        type: Number,
+                                        default: 0,
+                                },
+                                subscribersLost: {
+                                        type: Number,
+                                        default: 0,
+                                },
+                                likes: {
+                                        type: Number,
+                                        default: 0,
+                                },
+                                comments: {
+                                        type: Number,
+                                        default: 0,
+                                },
+                        },
+                ],
+        },
+        { timestamps: true }
+);
+
+// index for faster lookups
+channelAnalyticsSchema.index({ channel: 1, date: 1 });
+
+// model
+const ChannelAnalytics = mongoose.model("ChannelAnalytics", channelAnalyticsSchema);
+
+module.exports = ChannelAnalytics;
