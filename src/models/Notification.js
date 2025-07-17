a4ba2bb11ba43f47863c444273c5bc30a4970997
Phat117 <ntphat110705@gmail.com>
2025-07-17 21:29:18 +0700

    Notification model

---
+++ src/models/Notification.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+        {
+                recipent: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "User",
+                        required: [true, "recipent is required"],
+                },
+                sender: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "User",
+                        required: [true, "sender is required"],
+                },
+                type: {
+                        type: String,
+                        required: [true, "notification type is required"],
+                        enum: ["SUBSCRIPTION", "COMMENT", "REPLY", "SHARE", "VIDEO"],
+                },
+                content: {
+                        type: String,
+                        required: [true, "notification content is required"],
+                },
+                isRead: {
+                        type: Boolean,
+                        default: false,
+                },
+        },
+        { timestamps: true }
+);
+
+// Model
+const Notification = mongoose.model("Notification", notificationSchema);
+
+module.exports = Notification;
